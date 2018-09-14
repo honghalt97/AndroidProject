a89559a296bcbf7270e809b4c20774e0cb8a0cd3
phannerl <phanman7@gmail.com>
2018-09-14 09:09:07 +0700

    fea: Layout

---
+++ app/src/main/java/vell/layput_uiwiget/MainActivity.java
@@ -0,0 +1,13 @@
+package vell.layput_uiwiget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linearlayout_relativelayout_demo1);
+    }
+}
