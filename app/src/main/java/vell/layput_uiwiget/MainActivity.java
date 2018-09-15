bc8972a878a5510197e6032b7ec588718d5899ae
phannerl <phanman7@gmail.com>
2018-09-15 16:38:08 +0700

    feat: add facebook_clone

--- app/src/main/java/vell/layput_uiwiget/MainActivity.java
+++ app/src/main/java/vell/layput_uiwiget/MainActivity.java
@@ -1,13 +1,23 @@
 package vell.layput_uiwiget;
 
+import android.app.Activity;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.linearlayout_relativelayout_demo1);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.facebook__clone);
+
+        TextView textView = (TextView) findViewById(R.id.btn_sign_up);
+        textView.setPaintFlags(textView.getPaintFlags()|Paint.UNDERLINE_TEXT_FLAG);
+        TextView textView_forgot_pwd = (TextView) findViewById(R.id.btn_forget_passwd);
+        textView_forgot_pwd.setPaintFlags(textView_forgot_pwd.getPaintFlags()|Paint.UNDERLINE_TEXT_FLAG);
     }
 }
